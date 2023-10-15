509bfad4dec4cdcd649ec435aec1c8f8c44eee62
lcur0011 <147512231+lcur0011@users.noreply.github.com>
2023-10-15 23:26:09 +1100

    Add files via upload

--- a2.js
+++ a2.js
@@ -3,14 +3,15 @@ var idiom_2 = "map.json";
 var idiom_1 = "bar.json";
 var idiom_3 = "map2.json";
 var idiom_4 = "pie.json";
-var idiom_5 = "box.json"
+var idiom_5 = "box.json";
+var idiom_6 = "pies.json";
 
 vegaEmbed("#bar", idiom_1).then(function(result){}).catch(console.error);
 vegaEmbed("#mappy", idiom_2).then(function(result){}).catch(console.error);
 vegaEmbed("#map", idiom_3).then(function(result){}).catch(console.error);
 vegaEmbed("#pie", idiom_4).then(function(result){}).catch(console.error);
 vegaEmbed("#box", idiom_5).then(function(result){}).catch(console.error);
-
+vegaEmbed("#pies", idiom_6).then(function(result){}).catch(console.error);
 
 const spec1 = "map.json";
 const spec2 = "map2.json";
@@ -35,3 +36,4 @@ document.addEventListener('DOMContentLoaded', function () {
     vegaEmbed('#yourVegaContainer', spec1)
         .catch(console.error);
 });
+
